92034105d5f2ebc1a9e2af589563896dc85dbb93
spencerfolk <sfolk@seas.upenn.edu>
2025-01-03 16:48:05 -0500

    Implemented mppi ROS node structure.

--- wind_aware_mppi/include/wind_aware_mppi/mppi_controller_node.hpp
+++ wind_aware_mppi/include/wind_aware_mppi/mppi_controller_node.hpp
@@ -1,6 +1,53 @@
+#ifndef MPPI_CONTROLLER_NODE_HPP
+#define MPPI_CONTROLLER_NODE_HPP
+
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <geometry_msgs/PoseArray.h>
-#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <string>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+class MppiNode
+{
+
+public:
+
+    MppiNode(std::string ns);
+
+    void robotPoseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg);
+    void lidarCallback(const sensor_msgs::PointCloud2::ConstPtr &msg);
+    void runControl();
+    void publishCmdAcc();
+
+private:
+
+    // Robot parameters. 
+    std::string robot_name_; // Robot name for motion capture callback. 
+
+    // Controller parameters. 
+    Eigen::Vector3f cmd_acc_;   // Output commanded acceleration.
+    bool filter_control_;
+    bool use_traj_horizon_;
+    bool project_goal_;
+    bool normalize_costs_;
+    bool use_wind_;
+    bool adapt_goal_;
+    bool twod_;
+
+    // ROS and robot variables. 
+    ros::NodeHandle nh_;
+
+    ros::Subscriber robot_pose_sub_;
+    ros::Subscriber lidar_sub_;
+    ros::Publisher cmd_acc_pub_;
+
+    Eigen::Vector3f robot_pos_;
+    Eigen::Quaternionf robot_quat_;
+
+};
+
+
+#endif // MPPI_CONTROLLER_NODE_HPP
--- wind_aware_mppi/src/mppi_controller_node.cpp
+++ wind_aware_mppi/src/mppi_controller_node.cpp
@@ -1,5 +1,86 @@
 #include "wind_aware_mppi/mppi_controller_node.hpp"
 
+MppiNode::MppiNode(std::string ns) : nh_(ns)
+{
+    /*
+        Constructor
+    */
+
+   // Config variables. 
+   nh_.param("robot_name", robot_name_, std::string("robot"));
+
+   // Init subscribers. 
+   robot_pose_sub_ = nh_.subscribe("pose", 1, &MppiNode::robotPoseCallback, this);
+   lidar_sub_ = nh_.subscribe("lidar_raycast", 1, &MppiNode::lidarCallback, this);
+
+   // Init the publisher. 
+   cmd_acc_pub_ = nh_.advertise<geometry_msgs::Vector3Stamped>("mppi_cmd_acc", 1);
+
+}
+
+void MppiNode::robotPoseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
+    /*
+        Every time motion capture gives a new measurement of the robot's pose, save it as the current pose of the robot.
+    */
+
+    robot_pos_.x() = msg->pose.position.x;
+    robot_pos_.y() = msg->pose.position.y;
+    robot_pos_.z() = msg->pose.position.z;
+
+    robot_quat_.x() = msg->pose.orientation.x;
+    robot_quat_.y() = msg->pose.orientation.y;
+    robot_quat_.z() = msg->pose.orientation.z;
+    robot_quat_.w() = msg->pose.orientation.w;
+
+}
+
+void MppiNode::lidarCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+    /*
+        Every time the lidar produces a new scan, save the point cloud data for the controller. 
+    */
+
+    ROS_INFO_STREAM("Received LiDAR data...");
+
+}
+
+void MppiNode::runControl()
+{
+    /*
+        Run a control loop for the MPPI controller. 
+    */
+
+    // Run the control loop by feeding in the current pose of vehicle and the current time.
+    cmd_acc_ << robot_pos_.x(), robot_pos_.y(), robot_pos_.z(); // Temporarily report robot position. Replace with controller when implemented.
+
+    // Publish the commanded acceleration returned by the controller. 
+    publishCmdAcc();
+
+}
+
+void MppiNode::publishCmdAcc()
+{
+    /*
+        Publish the commanded acceleration vector from the MPPI controller. 
+    */
+
+    geometry_msgs::Vector3Stamped cmd_acc_msg;
+
+    // Header
+    cmd_acc_msg.header.stamp = ros::Time::now();
+    cmd_acc_msg.header.frame_id = "mocap";
+
+    // Set vector. 
+    cmd_acc_msg.vector.x = cmd_acc_.x();
+    cmd_acc_msg.vector.y = cmd_acc_.y();
+    cmd_acc_msg.vector.z = cmd_acc_.z();
+
+    // Publish the message
+    cmd_acc_pub_.publish(cmd_acc_msg);
+
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "mppi_node");
@@ -7,12 +88,13 @@ int main(int argc, char **argv)
     std::string ns = nh.getNamespace(); // Get namespace of the node
 
     // Create MPPI controller node object.
+    MppiNode mppi_node(ns);
 
     ros::Rate rate(50);
 
     while(ros::ok()  ){
-        // Publish wind measurement. 
-        ROS_INFO_STREAM("Wind Aware MPPI: Testing.");
+        // Run a single loop of the MPPI controller. 
+        mppi_node.runControl();
         ros::spinOnce();
         rate.sleep();
     }
